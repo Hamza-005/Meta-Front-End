38d3bc37cd3bfc1806965f660da132e319c1a141
Hamza-005 <hamzay471@outlook.com>
2025-04-28 21:13:14 +0300

    basics

---
+++ Course 2/javascript.js
@@ -0,0 +1,11 @@
+//the basics
+console.log("hello world");
+//logging in style
+console.log("%cHello, World", "color: blue; font-size: 40px");
+//the %c is to indicate styling before a string
+
+//variables
+
+var hamza;
+
+hamza = "Hamza";
