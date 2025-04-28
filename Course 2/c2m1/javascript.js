411bed79758203205eb2e8c3f2ec5126eaca55da
Hamza-005 <hamzay471@outlook.com>
2025-04-28 22:18:34 +0300

    c2m1

--- Course 2/c2m1/javascript.js
+++ Course 2/c2m1/javascript.js
@@ -11,29 +11,81 @@ var hamza;
 hamza = "Hamza";
 
 var greeting;
-greeting="Hello" + hamza;
+greeting = "Hello" + hamza;
 
-console.log("\n"+greeting);
+console.log("\n" + greeting);
 
-//first exersise of module 1
+
+
+//operators
+
+if (1 === 1) {
+    console.log("same type")
+} else {
+    console.log("not the same type")
+}
+
+
+
+
+
+//first exercise of module 1
 var petdog = "Rex";
-var petcat= "Pepper";
+var petcat = "Pepper";
 
 console.log(petdog);
 console.log(petcat);
 
-console.log("My pet dog's name is: "+petdog);
-console.log("My pet cat's name is: "+petcat);
+console.log("My pet dog's name is: " + petdog);
+console.log("My pet cat's name is: " + petcat);
 
-var catsound ="purr";
-var dogsound ="woof";
+var catsound = "purr";
+var dogsound = "woof";
 
 console.log(petdog + " says " + dogsound);
 console.log(petcat + " says " + catsound);
 
-catsound="meow";
+catsound = "meow";
 
 console.log(petcat + " now says " + catsound);
+
+console.log("I love", petcat, "so much");
+
+
+
+
+//second exercise module 1
+
+var score = 8;
+console.log("Mid-level skills:", score > 0 && score < 10)
+
+
+
+
+//another way to solve this would be
+if (score > 0 && score < 10) {
+    console.log("Mid-level skills:", true);
+} else {
+    console.log("Mid-level skills:", false);
+}
+
+
+
+
+//check if numbers are even or not using modulus
+
+function iseven(input) {
+    let temp = input;
+    if (temp % 2 == 0) {
+        console.log("Is", input, "an even number?", true)
+    } else {
+        console.log("Is", input, "an even number?", false)
+    }
+}
+
+iseven(2);
+iseven(5);
+
 
 
 
