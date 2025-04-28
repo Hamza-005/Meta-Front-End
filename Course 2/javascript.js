50336d52637ec5be6b047292bb9ccb4c7a552be4
Hamza-005 <hamzay471@outlook.com>
2025-04-28 21:32:46 +0300

    basics

--- Course 2/javascript.js
+++ Course 2/javascript.js
@@ -9,3 +9,30 @@ console.log("%cHello, World", "color: blue; font-size: 40px");
 //variables
 var hamza;
 hamza = "Hamza";
+
+var greeting;
+greeting="Hello" + hamza;
+
+console.log("\n"+greeting);
+
+var petdog = "Rex";
+var petcat= "Pepper";
+
+console.log(petdog);
+console.log(petcat);
+
+console.log("My pet dog's name is: "+petdog);
+console.log("My pet cat's name is: "+petcat);
+
+var catsound ="purr";
+var dogsound ="woof";
+
+console.log(petdog + " says " + dogsound);
+console.log(petcat + " says " + catsound);
+
+catsound="meow";
+
+console.log(petcat + " now says " + catsound);
+
+
+
