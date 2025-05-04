6564c70760a9f7701ee103c4994aeede24aaead5
Hamza-005 <hamzay471@outlook.com>
2025-05-04 19:28:38 +0300

    styling task

--- Course 2/c2m3/javascriptm3.js
+++ Course 2/c2m3/javascriptm3.js
@@ -12,4 +12,33 @@ function fib(n) {
 console.log(fib(5));
 
 
+function consoleStyler(color, background, fontSize, txt) {
+    var msg = "%c" + txt;
+    var style = `color: ${color};`
+    style += `background: ${background};`
+    style += `font-size: ${fontSize};`
+    console.log(msg, style)
+}
 
+function celebrateStyler(reason) {
+    var fontStyle = "color: tomato; font-size: 50px"
+    if (reason == "birthday") {
+        console.log(`%cHappy birthday`, fontStyle)
+    } else if (reason == "champions") {
+        console.log(`%cCongrats on the title!`, fontStyle)
+    } else {
+        console.log(msg, style)
+    }
+    
+}
+
+consoleStyler('#1d5c63', '#ede6db', '40px', 'Congrats!')
+celebrateStyler('birthday')
+
+function styleAndCelebrate(color, background, fontSize, txt, reason) {
+    consoleStyler(color, background, fontSize, txt)  
+    celebrateStyler(reason)
+    
+}
+
+styleAndCelebrate('ef7c8e', 'fae8e0', '30px', 'You made it!', 'champions');
